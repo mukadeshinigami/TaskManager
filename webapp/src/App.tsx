48f313bddab07bff5748fffb40c7e9d97f010ce5
mukadeshinigami <mukade.official@gmail.com>
2025-12-09 22:06:39 +0100

    add scss

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { TrpcProvider } from './lib/trpc'
 import { TaskManager } from './pages/TaskManager'
@@ -6,25 +6,19 @@ import { TaskMiniPage } from './pages/TaskMiniPage'
 import { TodoPage } from './pages/TodoPage'
 import { EditingTaskPage } from './pages/EditingTaskPage'
 import { Layout } from './components/Layout'
-import {
-  EditingTaskPageRoutes,
-  TaskManagerRoutes,
-  TaskMiniPageRoutes,
-  TodoPageRoutes,
-  ViewTaskPageRoutes,
-} from './lib/routes'
+import * as routes from './lib/routes'
 
 export default function App() {
   return (
     <TrpcProvider>
       <BrowserRouter>
-        <main>
+        <main>   
           <Routes>
             <Route path="/" element={<Layout />}>
-              <Route path={TaskManagerRoutes()} element={<TaskManager />} />
-              <Route path={TaskMiniPageRoutes(ViewTaskPageRoutes)} element={<TaskMiniPage />} />
-              <Route path={TodoPageRoutes()} element={<TodoPage />} />
-              <Route path={EditingTaskPageRoutes(ViewTaskPageRoutes)} element={<EditingTaskPage />} />
+              <Route path={routes.TaskManagerRoutes()} element={<TaskManager />} />
+              <Route path={routes.TaskMiniPageRoutes(routes.ViewTaskPageRoutes)} element={<TaskMiniPage />} />
+              <Route path={routes.TodoPageRoutes()} element={<TodoPage />} />
+              <Route path={routes.EditingTaskPageRoutes(routes.ViewTaskPageRoutes)} element={<EditingTaskPage />} />
             </Route>
           </Routes>
         </main>
